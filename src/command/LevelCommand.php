12219aa9d3821db34131272cd6abc9bc701cf3df
Mrchlldev <abimanyumarcel4@gmail.com>
2025-04-27 14:27:50 +0700

    first

---
+++ src/command/LevelCommand.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Mrchlldev\LevelSystem\command;
+
+use Mrchlldev\LevelSystem\manager\FormManager;
+use Mrchlldev\LevelSystem\LevelSystem;
+use CortexPE\Commando\BaseCommand;
+use CortexPE\Commando\args\RawStringArgument;
+use pocketmine\command\CommandSender
+use pocketmine\player\Player;
+
+class LevelCommand extends BaseCommand {
+
+    protected function prepare(): void {
+        $this->registerArgument(0, new RawStringArgument("player"));
+    }
+
+    public function onRun(CommandSender $sender, string $alias_used, array $args): void {
+        if (!$sender instanceof Player) {
+            $sender->sendMessage("Use this command in-game only!");
+            return;
+        }
+        if (!isset($args["player"])) {
+            FormManager::sendMenu($sender, $sender);
+            return;
+        }
+        $target = LevelSystem::getInstance()->getServer()->getPlayerExact($args["player"]);
+        if ($target == null) {
+            $sender->sendMessage(LevelSystem::PREFIX . "§cPlayer by name: §e" . $args["player"] . " §cnot found!");
+            return;
+        }
+        FormManager::sendMenu($sender, $target);
+    }
+}
---
+++ src/manager/FormManager.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Mrchlldev\LevelSystem\manager;
+
+use Mrchlldev\LevelSystem\manager\LevelManager;
+use Mrchlldev\LevelSystem\LevelSystem;
+use dktapps\pmforms\FormIcon;
+use dktapps\pmforms\MenuForm;
+use dktapps\pmforms\MenuOption;
+use dktapps\pmforms\ModalForm;
+use pocketmine\player\Player;
+
+class FormManager {
+
+    public function __construct(public static GenderSystem $plugin) {
+        // nope
+    }
+
+    public static function sendMenu(Player $player, Player $target): void {
+        $form = new MenuForm($target->getName() . "'s Level Information", "§a==========\n= §e» §aLevel: §e" . LevelManager::getLevelFromCache($target) . "\n§a= §e» §aXP: §e" . LevelManager::getExpFormattedFromCache($target) . "\n§a= §e» §aNext XP: §e" . LevelManager::getNextExpFormattedFromCache($target) . "\n§a==========", [
+            new MenuOption("§l§0» §cExit\n§rClick to exit")
+        ],
+        /**
+         * Called when player click/submit the form
+         * @param Player $player
+         * @param int $data
+         */
+        function (Player $player, int $data): void {
+            if ($data == 0) {
+                // gatau dah cuy
+            }
+        }, 
+        /**
+         * Called when player closed the form
+         * @param Player $player
+         */
+        function (Player $player): void {
+            // gatau dah cuy
+        });
+        $player->sendForm($form);
+    }
+}
