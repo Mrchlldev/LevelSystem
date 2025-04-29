1468288d99f4d507ae5dfc7c098348543e4dd150
Mrchlldev <137673915+Mrchlldev@users.noreply.github.com>
2025-04-29 08:27:21 +0700

    Add files via upload

--- src/command/LevelCommand.php
+++ src/command/LevelCommand.php
@@ -6,22 +6,19 @@ use Mrchlldev\LevelSystem\manager\FormManager;
 use Mrchlldev\LevelSystem\LevelSystem;
 use CortexPE\Commando\BaseCommand;
 use CortexPE\Commando\args\RawStringArgument;
-use pocketmine\command\CommandSender
+use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
 
 class LevelCommand extends BaseCommand {
 
     protected function prepare(): void {
         $this->registerArgument(0, new RawStringArgument("player"));
+        $this->setPermission("levelsystem.command.level");
     }
 
     public function onRun(CommandSender $sender, string $alias_used, array $args): void {
         if (!$sender instanceof Player) {
             $sender->sendMessage("Use this command in-game only!");
-            return;
-        }
-        if (!isset($args["player"])) {
-            FormManager::sendMenu($sender, $sender);
             return;
         }
         $target = LevelSystem::getInstance()->getServer()->getPlayerExact($args["player"]);
